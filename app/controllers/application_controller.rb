870235033366a0acfa73db9cddc7b5eb063f9267
“Ranjit <ranjit.saimbi@gmail.com>
2019-04-04 10:33:10 +0100

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,44 @@ class ApplicationController < Sinatra::Base
 
   get '/' do
   end
+
+  get '/articles/new' do
+    erb :new
+  end
+
+  get '/articles/:id' do
+    @article = Article.find_by(id: params[:id].to_i)
+    erb :show
+  end
+
+  patch '/articles/:id' do
+    article = Article.find_by(id: params[:id].to_i)
+    article.title = params[:title]
+    article.content = params[:content]
+    article.save
+
+    redirect "/articles/#{article.id}"
+  end
+
+  get '/articles' do
+    @articles = Article.all
+    erb :index
+  end
+
+  post '/articles' do
+    article = Article.create(title: params[:title], content: params[:content])
+    redirect "/articles/#{article.id}"
+  end
+
+  get '/articles/:id/edit' do
+    @article = Article.find_by(id: params[:id].to_i)
+    erb :edit
+  end
+
+  delete '/articles/:id' do
+    article = Article.find_by(id: params[:id].to_i)
+    article.destroy
+    redirect "/articles"
+  end
+
 end
